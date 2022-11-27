5c7a13b42f3e0495eb4bdb957c8d619a8b4ae41b
Mashhroma <mashh@mail.ru>
2022-11-27 18:13:07 +0300

    PROJECT 003

---
+++ PROJECT_003/Program.cs
@@ -0,0 +1,9 @@
+﻿// Это моя треться программа))) Делим рандомные числа)
+Console.WriteLine("Эта программа делит рандомные числа на 2:)");
+double a = new Random().Next(1, 1000);
+Console.Write ("Первое рандомное число: ");
+Console.WriteLine(a);
+Console.Write("Второе рандомное число: ");
+double b = 2;
+Console.WriteLine(b);
+Console.Write(a / b);
