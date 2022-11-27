c637eba5d0a12cc71d9e78b098c38c5dff8a1549
Mashhroma <mashh@mail.ru>
2022-11-27 18:46:19 +0300

    PROJECT 004

---
+++ PROJECT_004/Program.cs
@@ -0,0 +1,24 @@
+﻿// Это уже моя четвертая программа))))
+Console.Write("Введите Ваше имя: ");
+string name = Console.ReadLine();
+if(name.ToLower() == "пепси")
+{
+    Console.WriteLine("Привет, черный котик Пепси))))))");
+}
+else
+{
+    if(name.ToLower() == "Чубик")
+    {
+        Console.WriteLine("Привет, рыжий котик Чубик))))))");
+    }
+    else
+    {
+        if(name.ToLower() == "андрей")
+            Console.WriteLine("Привет, мой любимый Андрюшка))))))");
+        else
+        {
+            Console.Write("Привет, ");
+            Console.WriteLine(name);
+        }
+    }
+}
