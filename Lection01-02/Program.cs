ecc5073bb593dcd241b90d075b6f4b85f42d3151
Mashhroma <mashh@mail.ru>
2022-11-30 23:00:10 +0300

    add new projects

---
+++ Lection01-02/Program.cs
@@ -0,0 +1,21 @@
+﻿// Сравниваем 9 чисел и находим самое большое из них в массиве по индексу и по значению
+
+int[]MyNumbers = {9,2,4,5,6,88,5,33,24};
+
+int FindMax (int[]array)
+{
+    int MaxIndex = 0;
+    int index = 0;
+    int size = array.Length;
+    while (index < size)
+    {
+        if (array[MaxIndex] < array[index]) MaxIndex = index;
+        index++;
+    }
+    return MaxIndex;
+}
+
+int SuperMax = FindMax(MyNumbers);
+
+Console.WriteLine(SuperMax);
+Console.WriteLine(MyNumbers[SuperMax]);
---
+++ Lection02/Program.cs
@@ -0,0 +1,34 @@
+﻿// Простой метод, который возвращает самое большое число из трех
+
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int GetArg ()
+{
+    Console.Write("Введите число: ");
+    return int.Parse(Console.ReadLine()!);
+}
+
+Console.WriteLine("Данная программа сравнивает заданные числа и Возвращает самое большое из них!");
+
+int a1 = GetArg ();
+int b1 = GetArg ();
+int c1 = GetArg ();
+int a2 = GetArg ();
+int b2 = GetArg ();
+int c2 = GetArg ();
+int a3 = GetArg ();
+int b3 = GetArg ();
+int c3 = GetArg ();
+
+int SuperMax = Max(
+    Max(a1,b1,c1),
+    Max(a2,b2,c2),
+    Max(a3,b3,c3));
+
+Console.WriteLine($"Самое больше число: {SuperMax}");
