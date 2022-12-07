1c258db76e7d62df3fd92ec035f3b0316b3fd038
Mashhroma <mashh@mail.ru>
2022-12-07 23:29:46 +0300

    new tasks after lection 3

---
+++ Lection03-01/Program.cs
@@ -0,0 +1,21 @@
+﻿// Методы и строки
+
+string DiffText(string text, char oldSym, char newSym)
+{
+    string result = String.Empty;
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if (text[i] == oldSym) result = result + $"{newSym}";
+        else result = result + $"{text[i]}";
+    }
+    return result;
+}
+
+string myText = "Оборачиваемость товаров - это отношение скорости продажи товара к среднему запасу за определенный период."
+            + "Оборачиваемость может измеряться в разах (сколько раз за период товар успевает обернуться) или в днях"
+            + "(сколько дней нужно для продажи среднего запаса товара)";
+
+string NewText = DiffText(myText, 'ч', 'Ч');
+
+Console.WriteLine(NewText);
---
+++ Lection03-02/Program.cs
@@ -0,0 +1,50 @@
+﻿// Строчный палидром
+
+void PrintTurn (string text)
+{
+    int count = text.Length;
+    for (int i=0; i < text.Length; i++)
+    {
+        Console.WriteLine($"| Буква {i+1} - {text[i]} | Буква {count} - {text[count - 1]} | ");
+        count = count - 1;
+    }
+}
+
+string DelSpace (string text)
+{
+    string result = string.Empty;
+    for (int i=0; i < text.Length; i++)
+    {
+        if (text[i] != ' ') result = result + $"{text[i]}";
+    }
+    return result;
+}
+
+Console.Write("Введите слово, чтобы проверить является ли оно палиндромом: ");
+string wordAdd = Console.ReadLine()!;
+
+Console.WriteLine();
+
+string word = DelSpace(wordAdd);
+
+PrintTurn(word);
+
+int j = word.Length;
+
+for (int i = 0; i < word.Length; i++)
+{
+    if (word[i] != word[j - 1])
+    {
+        Console.WriteLine($"Данное слово/фраза {wordAdd} - это НЕ палиндром!");
+        break;
+    }
+    else if (word[i] == word[j - 1] && i > word.Length / 2)
+    {
+        Console.WriteLine($"Данное слово/фраза {wordAdd} - это палиндром!");
+        break;
+    }
+    else
+    {
+        j = j - 1;
+    }
+}
