a75b17ba154abde86aa82e47177897e129544517
Mashhroma <mashh@mail.ru>
2022-12-07 11:31:56 +0300

    add tasks of lection 2

---
+++ Lection02-02/Program.cs
@@ -0,0 +1,16 @@
+﻿int[]array = {1,12,22,4,53,57,22};
+
+int n = array.Length;
+int find = 22;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
